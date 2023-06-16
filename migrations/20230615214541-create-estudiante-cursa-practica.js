bd996a640012397498cd32d4f87cfaf8dc0db700
Vicente Balbontín Jara <vicente.balbontin@sansano.usm.cl>
2023-06-16 17:21:38 -0400

    newDB_sin_notnull

--- migrations/20230615214541-create-estudiante-cursa-practica.js
+++ migrations/20230615214541-create-estudiante-cursa-practica.js
@@ -63,11 +63,9 @@ module.exports = {
         type: Sequelize.STRING
       },
       createdAt: {
-        allowNull: false,
         type: Sequelize.DATE
       },
       updatedAt: {
-        allowNull: false,
         type: Sequelize.DATE
       }
     });
