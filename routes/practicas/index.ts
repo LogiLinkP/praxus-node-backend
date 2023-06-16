b098a11dbdbe2f8c78eda6f923c7aff01cd92410
Joacodef <joaquin.deferrari@sansano.usm.cl>
2023-06-16 16:46:34 -0400

    ruta para crear practicas, cambio nombre router consistencia

--- routes/practicas/index.ts
+++ routes/practicas/index.ts
@@ -2,7 +2,35 @@ export { };
 
 const { Router } = require('express');
 const routerPracticas = new Router();
+var bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const sequelize = require('../../db');
 
+// Import the axios library
+const axios = require('axios');
 
+// post para recibir request de calculo de consistencia desde el front
+routerPracticas.post('/crearPractica', jsonParser, (req: any, res: any) => {
+    const {nombre, tipo_practica, num_informes, cantidad_horas, modalidad} = req.body;
+    console.log("Request de creacion de practica recibida");
+    console.log(req.body);
+    // hacer post a python backend
+    sequelize.practica.create({
+        nombre: nombre,
+        tipo_practica: tipo_practica,
+        num_informes: num_informes,
+        horas: cantidad_horas,
+        modalidad: modalidad
+    })
+    .then((resultados:any) => {
+        console.log(resultados);
+    }
+    )
+    .catch((err:any) => {
+        console.log('Error al crear practica',err);
+    }
+    )
+    res.send("Practica creada");
+})
 
 module.exports = routerPracticas;
