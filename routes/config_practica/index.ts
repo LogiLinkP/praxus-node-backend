ea42ce55d35c986fad75fb9276d9421059d47986
Joacodef <joacodef1@gmail.com>
2023-08-09 16:44:31 -0400

    Quitado sequelize de config_informe y config_practica

--- routes/config_practica/index.ts
+++ routes/config_practica/index.ts
@@ -1,5 +1,6 @@
 export { };
 
+const { config_practica } = require('../../models');
 const { Router } = require('express');
 const sequelize = require('../../db');
 const routerConfigPracticas = new Router();
@@ -8,57 +9,60 @@ var bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 
-//[GET] mostrar todas las config_practicas
-routerConfigPracticas.get('/todos', (req:any, res:any) => {
-  console.log("Obteniendo todos los config_practica")
-  sequelize.config_practica.findAll().then((resultados:any) => {
-    res.send(resultados)
-  })
-  .catch((err:any) => {
-    console.log('Error al mostrar config_practica', err);
-    res.send('Error al mostrar config_practica', err)
-  })
-})
-
 //[GET] para obtener una config_practica con su ID
-routerConfigPracticas.get('', (req: any, res: any) => {
-    console.log("Obteniendo config_practica con id: ", req.query.id)
-    sequelize.config_practica.findOne({
-        where: {
-            id: req.query.id
-        }
-    })
-        .then((resultados: any) => {
-            res.send(resultados);
-        })
-        .catch((err: any) => {
-            console.log('Error al obtener config_practica', err);
-        })
-})
-
-//[DELETE] Eliminar config_practica con su ID
-routerConfigPracticas.delete('/eliminar', (req:any, res:any) => {
-    console.log("Eliminando config_practica con id: ", req.query.id)
-    sequelize.config_practica.destroy({
+routerConfigPracticas.get('', async (req: any, res: any) => {
+  try {
+    if (!("id" in req.query)) {
+      res.status(406).json({ message: "Se requiere ingresar id" });
+      return;
+    }
+    const data = await config_practica.findOne({
       where: {
         id: req.query.id
       }
-    })
-    .then((resultados:any) => {
+    });
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error interno" });
+  }
+});
+
+//[GET] mostrar todas las config_practicas
+routerConfigPracticas.get('/todos', async (req: any, res: any) => {
+  try {
+    const data = await config_practica.findAll();
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error interno" });
+  }
+});
+
+
+//[DELETE] Eliminar config_practica con su ID
+routerConfigPracticas.delete('/eliminar', (req: any, res: any) => {
+  console.log("Eliminando config_practica con id: ", req.query.id)
+  config_practica.destroy({
+    where: {
+      id: req.query.id
+    }
+  })
+    .then((resultados: any) => {
       console.log(resultados);
       res.sendStatus(200);
     })
-    .catch((err:any) => {
+    .catch((err: any) => {
       res.send(500)
       console.log('Error al eliminar config_practica', err);
     })
-  })
+})
 
 //[POST] Crear una config_practica con los datos recibidos
 routerConfigPracticas.post('/crear', jsonParser, (req: any, res: any) => {
     const {nombre, modalidad, cantidad_tiempo, frecuencia_informes,informe_final} = req.body;
     console.log("Request de creacion de config_practica recibida");
-    sequelize.config_practica.create({
+    config_practica.create({
         nombre: nombre,
         modalidad: modalidad,
         cantidad_tiempo: cantidad_tiempo,
@@ -75,21 +79,23 @@ routerConfigPracticas.post('/crear', jsonParser, (req: any, res: any) => {
 })
 
 //[PUT]
-routerConfigPracticas.put('/actualizar', jsonParser, async (req:any, res:any) => {
-  const config_practica = await sequelize.config_practica.findOne({ where: { id: req.body.id } })
-  if (config_practica){
-    config_practica.update(req.body)
-    .then((resultados:any) => {
-      console.log(resultados);
-      res.sendStatus(200);
-    })
-    .catch((err:any) => {
-      res.send(500)
-      console.log('Error al actualizar de config_practica', err);
-    })
+routerConfigPracticas.put('/actualizar', jsonParser, async (req: any, res: any) => {
+  // buscar practica por id
+  const Config_practica = await config_practica.findOne({ where: { id: req.body.id } })
+  if (Config_practica) {
+    // actualizar practica
+    Config_practica.update(req.body)
+      .then((resultados: any) => {
+        console.log(resultados);
+        res.sendStatus(200);
+      })
+      .catch((err: any) => {
+        res.send(500)
+        console.log('Error al actualizar config_practica', err);
+      })
   } else {
-      console.log("No existe config_practica con id: ", req.query.id)
-      res.sendStatus(404)
+    console.log("No existe config_practica con id: ", req.query.id)
+    res.sendStatus(404)
   }
 })
 
--- seeders/20230808223730-config_practica.js
+++ seeders/20230808223730-config_practica.js
@@ -5,18 +5,18 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('config_practica', [
       {
-        nombre: 'practica profesional',
+        nombre: 'Práctica Profesional',
         modalidad: 'meses',
         cantidad_tiempo: 1,
         frecuencia_informes: 'ninguna',
         informe_final: true,
       },
       {
-        nombre: 'practica industrial',
+        nombre: 'Práctica Industrial',
         modalidad: 'meses',
         cantidad_tiempo: 2,
         frecuencia_informes: 'semanal',
-        informe_final: true,
+        informe_final: false,
       }
     ], {});
   },
