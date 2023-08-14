5879c1f0a4bbf1b8fe073437c1198f0926d98d6c
Joacodef <joacodef1@gmail.com>
2023-08-13 22:19:55 -0400

    Instalado socket.io, codigo en index

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 let cors = require('cors')
+const chalk = require('chalk');
 const sequelize = require('./db');
 app.use(cors());
 
@@ -18,3 +19,35 @@ app.listen(PORT, function (err: any) {
     console.log('Error: ', err);
   })
 });
+
+const options = {
+  cors: {
+    origin: 'http://localhost:4200',
+  },
+};
+const server = require('http').Server(app);
+const io = require('socket.io')(server, options);
+
+io.on('connection', function (socket: any) {
+
+  const handshake = socket.id;
+
+  let { nameRoom } = socket.handshake.query;
+  console.log(`${chalk.green(`Nuevo dispositivo: ${handshake}`)} conentado a la ${nameRoom}`);
+  socket.join(nameRoom)
+
+  socket.on('evento', (res:any) => {
+    // Emite el mensaje a todos lo miembros de las sala menos a la persona que envia el mensaje   
+    socket.to(nameRoom).emit('evento', res);
+  })
+
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(5000, function () {
+  console.log('\n')
+  console.log(`>> Socket listo y escuchando por el puerto: ${chalk.green('5000')}`)
+})
