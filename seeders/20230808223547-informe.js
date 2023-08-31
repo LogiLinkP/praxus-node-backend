2ae219dc279f1504687e242d4c0d81bd7f3a49eb
Harold Caballero <mati_67m@hotmail.com>
2023-08-31 10:06:12 -0400

    quitadas comillas de seeders

--- seeders/20230808223547-informe.js
+++ seeders/20230808223547-informe.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('informe', [
       {
         id: 1,
@@ -10,7 +10,7 @@ module.exports = {
         id_config_informe: 1,
         horas_trabajadas: 0,
         fecha: '2023-06-11 23:59:59',
-        key : '{"1": "Yo participé de dos etapas principales, la primera ligada a la adquisición de conocimientos, herramientas y manejo de stack utilizado al interior de la empresa. Y la segunda orientada a participar en proyectos en proceso de desarrollo, con la toma de requerimientos a nivel de backend y frontend."}'
+        key: '{"1": "Yo participé de dos etapas principales, la primera ligada a la adquisición de conocimientos, herramientas y manejo de stack utilizado al interior de la empresa. Y la segunda orientada a participar en proyectos en proceso de desarrollo, con la toma de requerimientos a nivel de backend y frontend."}'
       },
       {
         id: 2,
@@ -18,12 +18,12 @@ module.exports = {
         id_config_informe: 2,
         horas_trabajadas: 0,
         fecha: '2023-06-11 23:59:59',
-        key : '{"2": "Hoy hice la presentación asignada el día anterior y escuché una presentación explicativa sobre “Gitflow”. Se nos presentó a nuestro tercer compañero de práctica y se nos dieron las credenciales para acceder al sistema de la empresa."}'
+        key: '{"2": "Hoy hice la presentación asignada el día anterior y escuché una presentación explicativa sobre Gitflow. Se nos presentó a nuestro tercer compañero de práctica y se nos dieron las credenciales para acceder al sistema de la empresa."}'
       }
     ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('informe', null, {});
   }
 };
