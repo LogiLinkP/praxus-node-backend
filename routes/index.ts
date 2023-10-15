d36ff713f1424447e2cc4cdbb3306664c5828d14
Marcelo Cabezas <marcelo.cabezas@sansano.usm.cl>
2023-10-15 12:10:25 -0300

    error de pusheo corregido

--- routes/index.ts
+++ routes/index.ts
@@ -32,6 +32,5 @@ router.use('/actualizacion_estadistica', require('./actualizacion_estadistica'))
 router.use('/carrera', require('./carrera'));
 router.use('/respuesta_ramos', require('./respuesta_ramos'));
 router.use('/publicacion', require('./publicacion'));
-router.use('/documento_encargado', require('./documento_encargado'))
 
 module.exports = router;
