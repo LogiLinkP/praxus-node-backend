bbbdc38e970beefc4d80433f3c08153f19471719
Marcelo Cabezas <marcelo.cabezas@sansano.usm.cl>
2023-10-21 15:29:02 -0300

    merge con dev, cron arreglado

--- scheduler/publicacion/index.ts
+++ scheduler/publicacion/index.ts
@@ -6,8 +6,11 @@ export async function publicaciones_programadas(frecuencia_informes: string) {
     //buscar en cada publicacion si la fecha programada ya se cumplió.
     let fecha = new Date();
     for (let publi of lista_publicaciones){
-        if(publi.dataValues.fecha_programada == null) continue; 
+        if(publi.dataValues.enviable == 1) continue; 
 
+        console.log("FECHA 1: ", publi.dataValues.fecha_programada)
+        console.log("FECHA 2: ", fecha)
+        console.log("Comparativa:", publi.dataValues.fecha_programada.getTime() <= fecha.getTime())
         if (publi.dataValues.fecha_programada.getTime() <= fecha.getTime()){
             await publi.update({enviable:true})
         }
