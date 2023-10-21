923125f24aaa77e86e9ddead4c7bd3971fedf552
Harold Caballero <mati_67m@hotmail.com>
2023-10-21 19:50:59 -0300

    agregados archivos borrados

---
+++ migrations/20231011190329-fecha-programada.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.addColumn('publicacions', 'fecha_programada', Sequelize.DATE);
+        return;
+    },
+
+    async down(queryInterface) {
+        await queryInterface.removeColumn('publicacions', 'fecha_programada');
+        return;
+    }
+};
