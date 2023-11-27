586dfab8eac1bf0ed248a4b22b75151f2fc70adb
Harold Caballero <mati_67m@hotmail.com>
2023-11-27 16:40:56 -0300

    modificada ruta test

--- routes/test/index.ts
+++ routes/test/index.ts
@@ -1,4 +1,5 @@
 export { };
+import xlsx from 'node-xlsx';
 
 const { informe } = require('../../models');
 const { Router } = require('express');
@@ -8,24 +9,42 @@ const jsonParser = bodyParser.json();
 
 routerTest.post('/agregar_informes', jsonParser, async (req: any, res: any) => {
     try {
-        const { fecha_ini, cant_dias, id_practica, id_config_informe } = req.query;
+        const file = xlsx.parse(`tmp/bitacoras_practicas.xlsx`);
+        // return res.json(file[0]["data"]);
+        const { fecha_ini, id_practica_file, id_practica, id_config_informe } = req.query;
         let rows = [];
         let date_0 = new Date(fecha_ini + " 03:00:00");
-        for (let i = 0; i < +cant_dias; i++) {
+        let idx = -1;
+        for (let i = 0; i < file[0]["data"].length; i++) {
+            if (file[0]["data"][i][0] == id_practica_file) {
+                idx = i;
+                break;
+            }
+
+        }
+        console.log(1)
+
+        let curr_row = file[0]["data"][idx];
+        while (curr_row[0] == id_practica_file) {
             rows.push({
                 id_practica, id_config_informe, horas_trabajadas: 0,
-                fecha: new Date(date_0)
+                fecha: new Date(date_0),
+                key: { 1: curr_row[2] }
             });
-            date_0.setDate(date_0.getDate() + 1)
+            date_0.setDate(date_0.getDate() + 1);
+            idx = (idx + 1) % file[0]["data"].length;
+            curr_row = file[0]["data"][idx];
         }
+        console.log(2)
         await informe.bulkCreate(rows);
+
         return res.json({ message: "filas agregadas" });
     } catch (error) {
         console.log(error);
-        return res.json({ message: "Ha ocurrido un error interno" })
+        return res.status(500).json({ message: "Ha ocurrido un error interno" })
     }
 
-})
+});
 
 
 module.exports = routerTest;
